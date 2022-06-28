1b0f06cfd59a1a0ac590db060ba93c5c0ff0807e
gyq <yq202222@163.com>
2022-06-28 23:55:34 +0800

    IDEA First Commit

---
+++ src/test01/HelloWorld.java
@@ -0,0 +1,21 @@
+/**
+ * @author gyq
+ * @version 1.0
+ */
+package test01;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello,world");
+        //单行注释
+        /*
+         多行注释
+         */
+        String userName;
+        final int MY_AGE = 18;
+    }
+}
+
+class person {
+
+}
